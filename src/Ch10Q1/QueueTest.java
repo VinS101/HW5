b2cd8dceb5736d3c3a0454ccd8b5f9ae69554506
VinS101 <shervinshahrdar@gmail.com>
2015-04-06 15:11:55 -0400

    Main class completed

--- src/Ch10Q1/QueueTest.java
+++ src/Ch10Q1/QueueTest.java
@@ -1,5 +1,8 @@
 package Ch10Q1;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * This class tests the queue 
  * 
@@ -8,6 +11,20 @@ public class QueueTest
 {
     public static void main(String[] args)
     {
+        LinkedList list = new LinkedList();
+        MyQueue queue = new LQueue(list);
+        
+        queue.enqueue(1);
+        queue.enqueue(2);
+        System.out.println( "Size: " + queue.size());
+        System.out.println("Head: " + queue.head());
+        System.out.println("Next Head: " + queue.dequeue());
+        
+        ArrayList collection = new ArrayList();
+        collection.add("a");
+        collection.add("b");
+        queue.addAll(collection);
+        System.out.println("Head of collection: " + queue.dequeue());
         
     }
 }
